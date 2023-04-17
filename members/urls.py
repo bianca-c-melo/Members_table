eda59da599f2a9619f2836a9fc217c1433f4b845
Bianca Camargo de Paula Melo <biancamelo@ibm.com>
2023-04-17 20:57:22 -0300

    update

--- members/urls.py
+++ members/urls.py
@@ -5,10 +5,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
    path('', views.index, name='index'),
   path('add/', views.add, name='add'),
-  path('add/addrecord/', views.addrecord, name='addrecord'),
+  path('add/add_record/', views.add_record, name='add_record'),
   path('delete/<int:id>', views.delete, name='delete'),
   path('update/<int:id>', views.update, name='update'),
-  path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord')
+  path('update/update_record/<int:id>', views.update_record, name='update_record')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- table_config/wsgi.py
+++ table_config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myworld project.
+WSGI config for members_table project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myworld.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'table_config.settings')
 
 application = get_wsgi_application()
 
